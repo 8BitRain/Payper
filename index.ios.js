6411ba351864bc8f4a0f162b5494f1dc9e4b9f1c
Eric <8BitRain@gmail.com>
2016-06-04 23:31:23 -0500

    Redux partially implemented

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,25 @@
 import { AppRegistry } from 'react-native';
+import React from 'react';
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 const Firebase = require('firebase');
 
 
 // @todo remove when RN upstream is fixed
 console.ignoredYellowBox = ['Warning: Failed propType: SceneView'];
 
-import Coincast from './Coincast';
+import CoincastContent from './Coincast';
+
+class Coincast extends React.Component{
+  render(){
+    return(
+      <Provider store={store}>
+        <CoincastContent/>
+      </Provider>
+    )
+  }
+}
 
 AppRegistry.registerComponent('Coincast', () => Coincast);
