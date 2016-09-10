a723aa4b8708df9088df4bc94ea93ec6aefdfabb
mrkhan2 <mohsink102@gmail.com>
2016-09-10 18:13:19 -0500

    Finalize merge

--- iOS/Coincast/AppDelegate.m
+++ iOS/Coincast/AppDelegate.m
@@ -47,7 +47,7 @@
    * on the same Wi-Fi network.
    */
 
-  // jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
@@ -57,13 +57,9 @@
    * simulator in the "Release" build configuration.
    */
 
-<<<<<<< HEAD
-  //jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-=======
 
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+//  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
->>>>>>> dwollaTest
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Coincast"
