16c03a2964d277510a7b5df5603b49f2f65aba45
Brady Sheridan <brady.sherid@gmail.com>
2016-09-19 13:54:32 -0500

    Fix font sizing issue with UserPic initials for smaller sizes

--- components/Previews/UserPic/UserPic.js
+++ components/Previews/UserPic/UserPic.js
@@ -21,7 +21,7 @@ class UserPic extends React.Component {
         : initials = name[0].substring(0, 1).toUpperCase();
       return(
         <View style={[userStyles.pic, userStyles.initials, {width: this.props.width, height: this.props.height, borderColor: (this.props.user.provider || this.props.user.username || this.props.accent) ? colors.accent : colors.icyBlue, borderRadius: this.props.width / 2}]}>
-          <Text style={{fontFamily: 'Roboto', fontSize: 18, color: (this.props.user.provider || this.props.user.username) ? colors.accent : colors.icyBlue}}>{ initials }</Text>
+          <Text style={{fontFamily: 'Roboto', fontSize: (this.props.width >= 50) ? 18 : 14, color: (this.props.user.provider || this.props.user.username) ? colors.accent : colors.icyBlue}}>{ initials }</Text>
         </View>
       );
     } else {
