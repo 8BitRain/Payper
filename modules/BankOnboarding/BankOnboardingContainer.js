5a072906e3a82bd000a21f136f96534790fc1e1b
Eric <8BitRain@gmail.com>
2016-09-30 18:39:32 -0500

    Add try catch around js error

--- modules/BankOnboarding/BankOnboardingContainer.js
+++ modules/BankOnboarding/BankOnboardingContainer.js
@@ -111,6 +111,7 @@ export default connect(
                    else Actions.MainViewContainer();
                  }catch(err2){
                    console.log("Error: " + err2);
+                   Actions.MainViewContainer();
                  }
 
                }
