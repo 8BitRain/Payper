535b8726560b4a1c25c0b8a0b02e807a52557217
Brady Sheridan <brady.sherid@gmail.com>
2016-10-22 18:56:52 -0500

    Finalize refreshable IAVWebView

--- components/IAVWebView/IAVWebView.js
+++ components/IAVWebView/IAVWebView.js
@@ -10,20 +10,19 @@ const dimensions = Dimensions.get('window');
 export default class IAVWebView extends React.Component {
   constructor(props) {
     super(props);
+
+    this.WEB_VIEW_REF = "IAVWebView";
     this.payperEnv = config.details.env;
     this.dwollaEnv = (config.details.env === "dev") ? "sandbox" : "prod";
+
     this.state = {
       cancelled: false,
-      IAVToken: this.props.IAVToken,
-      injectedJS: "var firebase_token = '" + this.props.firebaseToken + "';" +
-        "var iav_token = '" + this.props.IAVToken + "';" +
-        "$(function() { generateIAVToken(\"" + this.dwollaEnv + "\", \"" + this.payperEnv + "\") });"
-    }
-    this.WEB_VIEW_REF = "IAVWebView";
+      injectedJS: ""
+    };
   }
 
   componentWillMount() {
-    this.refreshIAVToken(() => this.refresh());
+    this.refresh();
     Mixpanel.timeEvent('IAV Onboarding');
   }
 
@@ -38,31 +37,38 @@ export default class IAVWebView extends React.Component {
 
   handleError(err) {
     console.log("Error loading WebView:\n", err);
-    this.refreshIAVToken(() => this.refresh());
   }
 
   refresh() {
-    this.refs[this.WEB_VIEW_REF].reload();
+    this.getIAVToken((IAVToken) => {
+      this.generateInjectedJS({
+        IAVToken: IAVToken,
+        firebaseToken: this.props.firebaseToken,
+        dwollaEnv: this.dwollaEnv,
+        payperEnv: this.payperEnv
+      }, () => {
+        this.refs[this.WEB_VIEW_REF].reload();
+      });
+    });
   }
 
-  refreshIAVToken(cb) {
+  getIAVToken(cb) {
     this.props.currentUser.getIAVToken({ token: this.props.currentUser.token }, (res) => {
-      console.log("New IAVToken:\n", res);
-      this.setState({
-        IAVToken: res.IAVToken,
-        injectedJS: "var firebase_token = '" + this.props.firebaseToken + "';" +
-          "var iav_token = '" + res.IAVToken + "';" +
-          "$(function() { generateIAVToken(\"" + this.dwollaEnv + "\", \"" + this.payperEnv + "\") });"
-      }, () => {
-        if (typeof cb === 'function') cb();
-      });
+      cb(res.IAVToken);
+    });
+  }
+
+  generateInjectedJS(params, cb) {
+    let injectedJS = "$(function() { generateIAVSession(\"" + params.IAVToken + "\", \"" + params.firebaseToken + "\", \"" + params.dwollaEnv + "\", \"" + params.payperEnv + "\"); })";
+    this.setState({ injectedJS: injectedJS }, () => {
+      if (typeof cb === 'function') cb();
     });
   }
 
   render() {
     if (this.props.refreshable) return(
       <View style={{ flex: 1.0, marginTop: 20 }}>
-        <View style={{ flex: 0.1, backgroundColor: colors.richBlack, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingLeft: 12.5, paddingRight: 12.5 }}>
+        <View style={{ flex: 0.1, backgroundColor: colors.richBlack, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingLeft: 12.5, paddingRight: 12.5, borderBottomWidth: 1.0, borderColor: colors.accent }}>
           { /* Header title */ }
           <View style={{ position: 'absolute', top: 0, left: 0, right: 0, height: dimensions.height * 0.1, justifyContent: 'center', alignItems: 'center' }}>
             <Text style={{ fontFamily: 'Roboto', fontSize: 16, fontWeight: '200', color: colors.white, textAlign: 'center' }}>
@@ -93,6 +99,9 @@ export default class IAVWebView extends React.Component {
           </TouchableHighlight>
         </View>
         <View style={{ flex: 0.9 }}>
+          { /* Background */ }
+          <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: colors.richBlack }} />
+
           <WebView
             ref={this.WEB_VIEW_REF}
             source={{ uri: 'http://www.getpayper.io/iav' }}
