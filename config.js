cac5e4578f59af0b5895ad7217f81770bd14820e
Brady Sheridan <brady.sherid@gmail.com>
2016-12-09 10:28:05 -0600

    Migrate codepush config from xcode proj to config.json

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ export function getBaseURL(cb) {
 }
 
 export var details = {
-  "env": "dev",
+  "env": "prod",
   "dev": {
     "lambdaBaseURL": "https://mey71fma7i.execute-api.us-east-1.amazonaws.com/dev/",
     "firebaseCredentials": {
@@ -39,7 +39,8 @@ export var details = {
       "authDomain": "coincast.firebaseapp.com",
       "databaseURL": "https://coincast.firebaseio.com",
       "storageBucket": "firebase-coincast.appspot.com"
-    }
+    },
+    "codePushKey": "AarYhu5gwn2uwqhzXiLJ3Rglm9MBVkksGQK3-"
   },
   "test": {
     "lambdaBaseURL": "https://mey71fma7i.execute-api.us-east-1.amazonaws.com/test/",
@@ -57,6 +58,7 @@ export var details = {
       "authDomain": "payper-prod.firebaseapp.com",
       "databaseURL": "https://payper-prod.firebaseio.com",
       "storageBucket": "payper-prod.appspot.com"
-    }
+    },
+    "codePushKey": "PhfwCJMZe1YxTjQq6ALKcf0XXO96VkksGQK3-"
   }
 };
