115a4009559510e9412f061662a85d20ec3348d2
Brady Sheridan <brady.sherid@gmail.com>
2016-12-19 14:25:09 -0600

    Fix a bug where bank accounts weren't being properly rendered

--- components/SideMenuSubpages/BankAccounts.js
+++ components/SideMenuSubpages/BankAccounts.js
@@ -32,7 +32,7 @@ class BankAccounts extends React.Component {
   }
 
   generateRows(currentUser) {
-    let rows = { "My Bank Accounts": (currentUser.bankAccount.name) ? [currentUser.bankAccount] : [{name: "None. Add one now!", notTouchable: true}] }
+    let rows = { "My Bank Accounts": (currentUser.bankAccount && currentUser.bankAccount.name) ? [currentUser.bankAccount] : [{name: "None. Add one now!", notTouchable: true}] }
     this.setState({rows: this.EMPTY_DATA_SOURCE.cloneWithRowsAndSections(rows)})
   }
 
