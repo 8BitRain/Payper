7c2ee32374ce287599533c5cc42fe91ff846eddd
Brady Sheridan <brady.sherid@gmail.com>
2017-01-22 14:31:00 -0600

    Incorporate input validation with paginate function
    "
    
    "

--- modules/NewUserOnboarding/NewUserOnboarding.js
+++ modules/NewUserOnboarding/NewUserOnboarding.js
@@ -1,9 +1,44 @@
 import React from 'react'
 import {Actions} from 'react-native-router-flux'
-import {View, Text, TouchableHighlight, Animated, Image} from 'react-native'
+import {View, Text, TouchableHighlight, Animated, Image, TextInput, Dimensions, StyleSheet, StatusBar, Alert} from 'react-native'
 import {colors} from '../../globalStyles'
 import Entypo from 'react-native-vector-icons/Entypo'
 import EvilIcons from 'react-native-vector-icons/EvilIcons'
+const dims = Dimensions.get('window')
+
+const styles = StyleSheet.create({
+  wrap: {
+    flex: 1.0,
+    alignItems: 'center',
+    paddingTop: 22
+  },
+  prompt: {
+    width: dims.width * 0.85,
+    fontSize: 20,
+    color: colors.deepBlue,
+    textAlign: 'center'
+  },
+  textInput: {
+    width: dims.width * 0.85,
+    paddingLeft: 14,
+    marginTop: 14,
+    backgroundColor: colors.lightGrey,
+    height: 44,
+    borderRadius: 4,
+    alignSelf: 'center'
+  },
+  continueButton: {
+    textAlign: 'center',
+    width: dims.width * 0.85,
+    marginTop: 15,
+    fontSize: 16,
+    color: colors.snowWhite,
+    backgroundColor: colors.gradientGreen,
+    padding: 14,
+    borderRadius: 4,
+    overflow: 'hidden'
+  }
+})
 
 class NewUserOnboarding extends React.Component {
   constructor(props) {
@@ -31,13 +66,27 @@ class NewUserOnboarding extends React.Component {
 
     this.state = {
       index: 0,
-      pressable: true
+      pressable: true,
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      phone: ""
     }
   }
 
   paginate(nextOrPrev) {
     let {index, pressable} = this.state
     let shouldPreventPagination = (nextOrPrev === "next" && index === this.pages.length - 1) || (nextOrPrev === "prev" && index === 0)
+
+    // Validate input
+    let inputIsValid = this.validate(this.pages[this.state.index])
+    if (nextOrPrev === "next" && !inputIsValid) {
+      Alert.alert('Invalid Input', `Please enter a valid ${this.pages[this.state.index]}.`)
+      return
+    }
+
+    // Prevent double animations and pagination overflow
     if (!pressable || shouldPreventPagination) return
 
     this.setState({pressable: false})
@@ -70,9 +119,39 @@ class NewUserOnboarding extends React.Component {
     })
   }
 
+  onChangeText(text, label) {
+    this.state[label] = text
+    this.setState(() => console.log(this.state))
+  }
+
+  validate(label) {
+    let valid = false
+
+    console.log("--> label", label)
+
+    switch (label) {
+      case "name":
+        valid = this.state.firstName.length > 0 && this.state.lastName.length > 0;
+        break;
+      case "email":
+        valid = this.state.email.length > 0;
+        break;
+      case "password":
+        valid = this.state.password.length > 0;
+        break;
+      case "phone":
+        valid = this.state.phone.length > 0;
+        break;
+    }
+
+    return valid
+  }
+
   render() {
     return(
       <View style={{flex: 1.0, backgroundColor: colors.snowWhite}}>
+        <StatusBar barStyle='light-content' />
+
         { /* Header */ }
         <View style={{overflow: 'hidden'}}>
           <Image source={require('../../assets/images/bg-header.jpg')} style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}} />
@@ -100,16 +179,36 @@ class NewUserOnboarding extends React.Component {
         </View>
 
         { /* Content wrap*/ }
-        <View style={{flex: 1.0, borderWidth: 1, borderColor: 'red'}}>
+        <View style={{flex: 1.0}}>
           { /* Name */
             (this.pages[this.state.index] !== "name")
               ? null
-              : <Animated.View style={{opacity: this.AV.name.opacity, position: 'absolute', top: 0, right: 0, left: 0, bottom: 0, flex: 1.0, backgroundColor: 'blue'}}>
-                  <TouchableHighlight
-                    activeOpacity={0.75}
-                    underlayColor={'transparent'}
-                    onPress={() => this.paginate("next")}>
-                    <EvilIcons name={"chevron-right"} color={colors.snowWhite} size={30} />
+              : <Animated.View style={[styles.wrap, {opacity: this.AV.name.opacity}]}>
+                  <Text style={styles.prompt}>
+                    {"What's your name?"}
+                  </Text>
+                  <TextInput
+                    ref={"firstNameInput"}
+                    autoFocus autoCorrect={false}
+                    defaultValue={this.state.firstName}
+                    placeholder={"First name"}
+                    style={styles.textInput}
+                    returnKeyType={"next"}
+                    onSubmitEditing={() => this.refs.lastNameInput.focus()}
+                    onChangeText={(text) => this.onChangeText(text, "firstName")} />
+                  <TextInput
+                    ref={"lastNameInput"}
+                    autoCorrect={false}
+                    defaultValue={this.state.lastName}
+                    placeholder={"Last name"}
+                    style={styles.textInput}
+                    returnKeyType={"done"}
+                    onSubmitEditing={() => this.paginate("next")}
+                    onChangeText={(text) => this.onChangeText(text, "lastName")} />
+                  <TouchableHighlight underlayColor={'transparent'} onPress={() => this.paginate("next")}>
+                    <Text style={styles.continueButton}>
+                      {"Continue"}
+                    </Text>
                   </TouchableHighlight>
                 </Animated.View> }
 
