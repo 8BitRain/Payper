a39efa9c9972fb18339d217da4a774926cb07740
Eric <8BitRain@gmail.com>
2017-02-23 08:09:50 -0600

    Setting up enviroment for push notifications

--- android/app/src/main/java/com/example/MainApplication.java
+++ android/app/src/main/java/com/example/MainApplication.java
@@ -2,6 +2,7 @@ package com.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.evollu.react.fcm.FIRMessagingPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -41,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
             return Arrays.<ReactPackage>asList(
                     new RNMixpanel(),
                     new MainReactPackage(),
+            new FIRMessagingPackage(),
             new RNDeviceInfo(),
                     new FabricPackage(),
                     new CodePush("PhfwCJMZe1YxTjQq6ALKcf0XXO96VkksGQK3", MainApplication.this, com.example.BuildConfig.DEBUG),
