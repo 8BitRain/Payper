7a94f230702ffb5d02077961b2da6abf2994175a
bradysheridan <brady.sherid@gmail.com>
2017-06-21 13:51:18 -0500

    Fix a bug where 'friendNetwork' type casts wouldn't show up in broadcast feed due to a typo in the broadcast listener code

--- classes/User.js
+++ classes/User.js
@@ -201,7 +201,7 @@ export default class User {
             // Sort castID into appropriate section of broadcastFeedIDs
             if (type === "world") broadcastFeedIDs["Global"].push(castID)
             else if (type === "local") broadcastFeedIDs["Local"].push(castID)
-            else if (type === "friendnetwork") broadcastFeedIDs["Friends of Friends"].push(castID)
+            else if (type === "friendNetwork") broadcastFeedIDs["Friends of Friends"].push(castID)
             else if (type === "friends") broadcastFeedIDs["Friends"].push(castID)
 
             // Add Firebase binding
